c5a19d6bbe3190b3dd3b562dcd6d2e8962045b5e
Ruiner85 <paul.m.mackay85@gmail.com>
2020-01-26 20:46:49 +0000

    add bar/till test

--- bar.rb
+++ bar.rb
@@ -6,4 +6,10 @@ def initialize(till)
   @till = till
 end
 
+def from_guest_to_till(guest, fee)
+  guest.wallet - fee
+  till_total = @till += fee
+  return till_total
+end
+
 end
--- specs/bar_spec.rb
+++ specs/bar_spec.rb
@@ -4,9 +4,22 @@ require("minitest/reporters")
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative("../bar")
+require_relative("../guests")
 
 class BarTest < Minitest::Test
 
 def setup
  @bar = Bar.new(100)
+end
+
+def test_bar_has_cash
+  assert_equal(100, @bar.till)
+end
+
+def test_money_from_guest_to_till
+  @guest1 = Guest.new("Paul", 20, "The Only Way Is Up")
+  @bar.from_guest_to_till(@guest1, 10)
+  assert_equal(110, @bar.till)
+end
+
 end
